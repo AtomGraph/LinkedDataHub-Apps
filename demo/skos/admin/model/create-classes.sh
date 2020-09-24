59d4895df4d6a6e3f69db8ccd710d1726558544e
Martynas <martynas@atomgraph.com>
2020-09-24 20:07:17 +0200

    Fixed usage hint output in scripts

--- demo/skos/admin/model/create-classes.sh
+++ demo/skos/admin/model/create-classes.sh
@@ -3,7 +3,7 @@
 [ -z "$SCRIPT_ROOT" ] && echo "Need to set SCRIPT_ROOT" && exit 1;
 
 if [ "$#" -ne 3 ]; then
-  echo "Usage:   $0 $base $cert_pem_file $cert_password" >&2
+  echo "Usage:   $0" '$base $cert_pem_file $cert_password' >&2
   echo "Example: $0" 'https://localhost:4443/demo/skos/ ../../../../../certs/martynas.stage.localhost.pem Password' >&2
   echo "Note: special characters such as $ need to be escaped in passwords!" >&2
   exit 1
--- demo/skos/create-containers.sh
+++ demo/skos/create-containers.sh
@@ -3,7 +3,7 @@
 [ -z "$SCRIPT_ROOT" ] && echo "Need to set SCRIPT_ROOT" && exit 1;
 
 if [ "$#" -ne 3 ]; then
-  echo "Usage:   $0 $base $cert_pem_file $cert_password" >&2
+  echo "Usage:   $0" '$base $cert_pem_file $cert_password' >&2
   echo "Example: $0" 'https://linkeddatahub.com/atomgraph/app/ ../../../certs/martynas.localhost.pem Password' >&2
   echo "Note: special characters such as $ need to be escaped in passwords!" >&2
   exit 1
