c5fe52fe238128830bf6dc77280342c5fa636001
Martynas <martynas@atomgraph.com>
2021-03-01 14:02:45 +0100

    Accept $request_base in import-rdf.sh

--- demo/skos/import-rdf.sh
+++ demo/skos/import-rdf.sh
@@ -2,8 +2,8 @@
 
 [ -z "$SCRIPT_ROOT" ] && echo "Need to set SCRIPT_ROOT" && exit 1;
 
-if [ "$#" -ne 3 ]; then
-  echo "Usage:   $0" '$base $cert_pem_file $cert_password' >&2
+if [ "$#" -ne 3 ] && [ "$#" -ne 4 ]; then
+  echo "Usage:   $0" '$base $cert_pem_file $cert_password [$request_base]' >&2
   echo "Example: $0" 'https://linkeddatahub.com/atomgraph/app/ ../../../certs/martynas.localhost.pem Password' >&2
   echo "Note: special characters such as $ need to be escaped in passwords!" >&2
   exit 1
@@ -13,6 +13,12 @@ base="$1"
 cert_pem_file=$(realpath -s "$2")
 cert_password="$3"
 
+if [ -n "$4" ]; then
+    request_base="$4"
+else
+    request_base="$base"
+fi
+
 pwd=$(realpath -s "$PWD")
 
 pushd . && cd "$SCRIPT_ROOT"/imports
@@ -21,6 +27,7 @@ pushd . && cd "$SCRIPT_ROOT"/imports
 -b "$base" \
 -f "$cert_pem_file" \
 -p "$cert_password" \
+--request-base "$request_base" \
 --title "UN thesaurus SKOS" \
 --query-file "$pwd/queries/skos-import.rq" \
 --file "$pwd/files/unesco-thesaurus.ttl" \
