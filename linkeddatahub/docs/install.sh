39f26c9a026f24c069beaada1b4709a9cada3a8f
Martynas <martynas@atomgraph.com>
2021-03-02 11:14:34 +0100

    $request_base support in linkeddatahub install scripts

--- linkeddatahub/docs/install.sh
+++ linkeddatahub/docs/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ]; then
-  echo "Usage:   $0" '$base $cert_pem_file $cert_password' >&2
+if [ "$#" -ne 3 ] && [ "$#" -ne 4 ]; then
+  echo "Usage:   $0" '$base $cert_pem_file $cert_password [$request_base]' >&2
   echo "Example: $0" 'https://localhost:4443/ ../../../LinkedDataHub/certs/owner.p12.pem Password' >&2
   echo "Note: special characters such as $ need to be escaped in passwords!" >&2
   exit 1
@@ -10,24 +10,31 @@ fi
 base="$1"
 cert_pem_file=$(realpath -s "$2")
 cert_password="$3"
+
+if [ -n "$4" ]; then
+    request_base="$4"
+else
+    request_base="$base"
+fi
+
 pwd="$(realpath -s "$PWD")"
 
 printf "\n### Creating authorization to make the app public\n\n"
 
-"$SCRIPT_ROOT"/admin/acl/make-public.sh -b "$base" -f "$cert_pem_file" -p "$cert_password"
+"$SCRIPT_ROOT"/admin/acl/make-public.sh -b "$base" -f "$cert_pem_file" -p "$cert_password" --request-base "$request_base"
 
 printf "\n### Creating authorizations\n\n"
 
-find "${pwd}" -name '*.ttl' -exec ./admin/acl/create-authorization.sh "${base}" "${cert_pem_file}" "${cert_password}" "${pwd}" {} \;
+find "${pwd}" -name '*.ttl' -exec ./admin/acl/create-authorization.sh "${base}" "${cert_pem_file}" "${cert_password}" "${pwd}" {} "$request_base" \;
 
 printf "\n### Update documents\n\n"
 
-find "${pwd}" -name '*.ttl' -exec ./update-document.sh "${base}" "${cert_pem_file}" "${cert_password}" "${pwd}" {} \;
+find "${pwd}" -name '*.ttl' -exec ./update-document.sh "${base}" "${cert_pem_file}" "${cert_password}" "${pwd}" {} "$request_base" \;
 
 printf "\n### Uploading images\n\n"
 
-find "${pwd}/files/images" -type f -exec ./upload-file.sh "${base}" "${cert_pem_file}" "${cert_password}" "${pwd}" {} \;
+find "${pwd}/files/images" -type f -exec ./upload-file.sh "${base}" "${cert_pem_file}" "${cert_password}" "${pwd}" {} "$request_base" \;
 
 printf "\n### Uploading videos\n\n"
 
-find "${pwd}/files/videos" -type f -exec ./upload-file.sh "${base}" "${cert_pem_file}" "${cert_password}" "${pwd}" {} \;
+find "${pwd}/files/videos" -type f -exec ./upload-file.sh "${base}" "${cert_pem_file}" "${cert_password}" "${pwd}" {} "$request_base" \;
--- linkeddatahub/docs/update-document.sh
+++ linkeddatahub/docs/update-document.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$#" -ne 5 ]; then
-  echo "Usage:   $0" '$cert_pem_file $cert_password $pwd $abs_filename' >&2
+if [ "$#" -ne 5 ] && [ "$#" -ne 6 ]; then
+  echo "Usage:   $0" '$base $cert_pem_file $cert_password $pwd $abs_filename [$request_base]' >&2
   echo "Example: $0" 'https://linkeddatahub.com/my-context/my-dataspace/ ../../certs/martynas.stage.localhost.pem Password /folder /folder/file.ttl' >&2
   echo "Note: special characters such as $ need to be escaped in passwords!" >&2
   exit 1
@@ -12,6 +12,12 @@ cert_pem_file="$2"
 cert_password="$3"
 pwd="$4"
 filename="$5"
+
+if [ -n "$6" ]; then
+    request_base="$6"
+else
+    request_base="$base"
+fi
 
 path="${filename%.*}" # strip the extension
 path="${path#*$pwd/}" # strip the leading $pwd/
@@ -25,6 +31,6 @@ cat "${filename}" | turtle --base="${base}" | "$SCRIPT_ROOT"/update-document.sh 
 -f "$cert_pem_file" \
 -p "$cert_password" \
 -t "application/n-triples" \
-"${base}${path}"
+"${request_base}${path}"
 
 popd
