4e22b95d0d7781f8042dcee48408ca165891018c
Martynas <martynas@atomgraph.com>
2022-05-19 13:24:21 +0200

    Refactored Documentation install scripts using --proxy

--- linkeddatahub/docs/install.sh
+++ linkeddatahub/docs/install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [ "$#" -ne 3 ] && [ "$#" -ne 4 ]; then
-  echo "Usage:   $0" '$base $cert_pem_file $cert_password [$request_base]' >&2
-  echo "Example: $0" 'https://localhost:4443/ ../../../LinkedDataHub/ssl/owner/cert.pem Password' >&2
+  echo "Usage:   $0" '$base $cert_pem_file $cert_password [$proxy]' >&2
+  echo "Example: $0" 'https://localhost:4443/ ../../../LinkedDataHub/ssl/owner/cert.pem Password [https://localhost:5443/]' >&2
   echo "Note: special characters such as $ need to be escaped in passwords!" >&2
   exit 1
 fi
@@ -12,29 +12,29 @@ cert_pem_file=$(realpath -s "$2")
 cert_password="$3"
 
 if [ -n "$4" ]; then
-    request_base="$4"
+    proxy="$4"
 else
-    request_base="$base"
+    proxy="$base"
 fi
 
 pwd="$(realpath -s "$PWD")"
 
 printf "\n### Creating authorization to make the app public\n\n"
 
-"$SCRIPT_ROOT"/admin/acl/make-public.sh -b "$base" -f "$cert_pem_file" -p "$cert_password" --proxy "$request_base"
+"$SCRIPT_ROOT"/admin/acl/make-public.sh -b "$base" -f "$cert_pem_file" -p "$cert_password" --proxy "$proxy"
 
 printf "\n### Creating authorizations\n\n"
 
-find "${pwd}" -name '*.ttl' -exec ./admin/acl/create-authorization.sh "${base}" "${cert_pem_file}" "${cert_password}" "${pwd}" {} "$request_base" \;
+find "${pwd}" -name '*.ttl' -exec ./admin/acl/create-authorization.sh "${base}" "${cert_pem_file}" "${cert_password}" "${pwd}" {} "$proxy" \;
 
 printf "\n### Update documents\n\n"
 
-find "${pwd}" -name '*.ttl' -exec ./update-document.sh "${base}" "${cert_pem_file}" "${cert_password}" "${pwd}" {} "$request_base" \;
+find "${pwd}" -name '*.ttl' -exec ./update-document.sh "${base}" "${cert_pem_file}" "${cert_password}" "${pwd}" {} "$proxy" \;
 
 printf "\n### Uploading images\n\n"
 
-find "${pwd}/files/images" -type f -exec ./upload-file.sh "${base}" "${cert_pem_file}" "${cert_password}" "${pwd}" {} "$request_base" \;
+find "${pwd}/files/images" -type f -exec ./upload-file.sh "${base}" "${cert_pem_file}" "${cert_password}" "${pwd}" {} "$proxy" \;
 
 printf "\n### Uploading videos\n\n"
 
-find "${pwd}/files/videos" -type f -exec ./upload-file.sh "${base}" "${cert_pem_file}" "${cert_password}" "${pwd}" {} "$request_base" \;
+find "${pwd}/files/videos" -type f -exec ./upload-file.sh "${base}" "${cert_pem_file}" "${cert_password}" "${pwd}" {} "$proxy" \;
--- linkeddatahub/docs/update-document.sh
+++ linkeddatahub/docs/update-document.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [ "$#" -ne 5 ] && [ "$#" -ne 6 ]; then
-  echo "Usage:   $0" '$base $cert_pem_file $cert_password $pwd $abs_filename [$request_base]' >&2
-  echo "Example: $0" 'https://localhost:4443/ ../../ssl/owner/cert.pem Password /folder /folder/file.ttl' >&2
+  echo "Usage:   $0" '$base $cert_pem_file $cert_password $pwd $abs_filename [$proxy]' >&2
+  echo "Example: $0" 'https://localhost:4443/ ../../ssl/owner/cert.pem Password /folder /folder/file.ttl [https://localhost:5443/]' >&2
   echo "Note: special characters such as $ need to be escaped in passwords!" >&2
   exit 1
 fi
@@ -14,9 +14,9 @@ pwd="$4"
 filename="$5"
 
 if [ -n "$6" ]; then
-    request_base="$6"
+    proxy="$6"
 else
-    request_base="$base"
+    proxy="$base"
 fi
 
 path="${filename%.*}" # strip the extension
@@ -28,9 +28,10 @@ pushd . && cd "$SCRIPT_ROOT"
 printf "\n### Updating %s\n" "${base}${path}"
 
 cat "${filename}" | turtle --base="${base}" | "$SCRIPT_ROOT"/update-document.sh \
--f "$cert_pem_file" \
--p "$cert_password" \
--t "application/n-triples" \
-"${request_base}${path}"
+  -f "$cert_pem_file" \
+  -p "$cert_password" \
+  --proxy "$proxy" \
+  -t "application/n-triples" \
+  "${base}${path}"
 
 popd
