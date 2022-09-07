849478bb2d8062d73dd772d1948273b1799d817f
Martynas <martynas@atomgraph.com>
2022-09-07 18:33:52 +0200

    Added missing update-documents.sh

---
+++ demo/city-graph/update-documents.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+[ -z "$SCRIPT_ROOT" ] && echo "Need to set SCRIPT_ROOT" && exit 1;
+
+if [ "$#" -ne 3 ] && [ "$#" -ne 4 ]; then
+  echo "Usage:   $0" '$base $cert_pem_file $cert_password [$proxy]' >&2
+  echo "Example: $0" 'https://localhost:4443/ ../../../ssl/owner/cert.pem Password [https://localhost:5443/]' >&2
+  echo "Note: special characters such as $ need to be escaped in passwords!" >&2
+  exit 1
+fi
+
+base="$1"
+cert_pem_file="$2"
+cert_password="$3"
+
+if [ -n "$4" ]; then
+    proxy="$4"
+else
+    proxy="$base"
+fi
+
+cat root.ttl | turtle --base="${base}" | "$SCRIPT_ROOT"/update-document.sh \
+  -f "$cert_pem_file" \
+  -p "$cert_password" \
+  --proxy "$proxy" \
+  -t "application/n-triples" \
+  "$base"
