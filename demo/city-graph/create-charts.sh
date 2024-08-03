c6a5f3eb2b4a3972813a87d08fd3fd506ae60c72
Martynas <martynas@atomgraph.com>
2024-08-03 11:37:07 +0200

    Updating scripts to use LDH v5 CLI

--- demo/city-graph/create-charts.sh
+++ demo/city-graph/create-charts.sh
@@ -23,16 +23,36 @@ pwd=$(realpath -s "$PWD")
 
 pushd . && cd "$SCRIPT_ROOT"
 
-query_doc=$(
-./create-select.sh  \
+chart_container=$(./create-container.sh \
+  -b "$base" \
+  -f "$cert_pem_file" \
+  -p "$cert_password" \
+  --proxy "$proxy" \
+  --title "Charts" \
+  --slug "charts" \
+  --parent "$parent" \
+  --mode "https://w3id.org/atomgraph/client#MapMode"
+)
+
+parking_facilities_doc=$(./create-item.sh \
+  -b "${base}admin/" \
+  -f "$cert_pem_file" \
+  -p "$cert_password" \
+  --proxy "$proxy" \
+  --title "Parking facilities" \
+  --slug "parking-facilities" \
+  --container "$chart_container"
+)
+
+./add-select.sh  \
   -b "$base" \
   -f "$cert_pem_file" \
   -p "$cert_password" \
   --proxy "$proxy" \
   --title "Largest parking facilities" \
+  --fragment "parking-facilities-by-spaces" \
   --query-file "${pwd}/queries/charts/parking-facilities-by-spaces.rq"
-  #--fragment this \
-)
+  "$parking_facilities_doc"
 
 pushd . > /dev/null && cd "$SCRIPT_ROOT"
 
