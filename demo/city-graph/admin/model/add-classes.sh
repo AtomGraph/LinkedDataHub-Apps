642052c5cabfc67826960304c61c8f5b47930466
Martynas Jusevičius <martynas@atomgraph.com>
2025-06-24 11:12:48 +0200

    City Graph script updates

--- demo/city-graph/admin/model/add-classes.sh
+++ demo/city-graph/admin/model/add-classes.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-[ -z "$SCRIPT_ROOT" ] && echo "Need to set SCRIPT_ROOT" && exit 1;
-
 if [ "$#" -ne 3 ] && [ "$#" -ne 4 ]; then
   echo "Usage:   $0" '$base $cert_pem_file $cert_password [$proxy]' >&2
   echo "Example: $0" 'https://localhost:4443/demo/city-graph/ ../../../../../certs/martynas.stage.localhost.pem Password [https://localhost:5443/]' >&2
@@ -19,105 +17,92 @@ else
     proxy="$base"
 fi
 
-pushd . && cd "$SCRIPT_ROOT/admin/model"
-
-./add-class.sh \
+add-class.sh \
   -b "${base}admin/" \
   -f "$cert_pem_file" \
   -p "$cert_password" \
   --proxy "$proxy" \
   --uri "${base}ns#BicycleParking" \
   --label "Bicycle parking" \
-  --slug bicycle-parking \
   --sub-class-of "https://schema.org/CivicStructure" \
   "${base}admin/ontologies/namespace/"
 
-./add-class.sh \
+add-class.sh \
   -b "${base}admin/" \
   -f "$cert_pem_file" \
   -p "$cert_password" \
   --proxy "$proxy" \
   --uri "${base}ns#ChargingStation" \
   --label "Charging station" \
-  --slug charging-station \
   --sub-class-of "https://schema.org/CivicStructure" \
   "${base}admin/ontologies/namespace/"
 
-./add-class.sh \
+add-class.sh \
   -b "${base}admin/" \
   -f "$cert_pem_file" \
   -p "$cert_password" \
     --proxy "$proxy" \
   --uri "${base}ns#Library" \
   --label "Library" \
-  --slug library \
   --sub-class-of "https://schema.org/Library" \
   "${base}admin/ontologies/namespace/"
 
-./add-class.sh \
+add-class.sh \
   -b "${base}admin/" \
   -f "$cert_pem_file" \
   -p "$cert_password" \
   --proxy "$proxy" \
   --uri "${base}ns#ParkingFacility" \
   --label "Parking facility" \
-  --slug parking-facility \
   --sub-class-of "https://schema.org/ParkingFacility" \
   "${base}admin/ontologies/namespace/"
 
-./add-class.sh \
+add-class.sh \
   -b "${base}admin/" \
   -f "$cert_pem_file" \
   -p "$cert_password" \
   --proxy "$proxy" \
   --uri "${base}ns#Place" \
   --label "Place" \
-  --slug place \
   --sub-class-of "https://schema.org/CivicStructure" \
   "${base}admin/ontologies/namespace/"
 
-./add-class.sh \
+add-class.sh \
   -b "${base}admin/" \
   -f "$cert_pem_file" \
   -p "$cert_password" \
   --proxy "$proxy" \
   --uri "${base}ns#Playground" \
   --label "Playground" \
-  --slug playground \
   --sub-class-of "https://schema.org/Playground" \
   "${base}admin/ontologies/namespace/"
 
-./add-class.sh \
+add-class.sh \
   -b "${base}admin/" \
   -f "$cert_pem_file" \
   -p "$cert_password" \
   --proxy "$proxy" \
   --uri "${base}ns#School" \
   --label "School" \
-  --slug school \
   --sub-class-of "https://schema.org/School" \
   "${base}admin/ontologies/namespace/"
 
-./add-class.sh \
+add-class.sh \
   -b "${base}admin/" \
   -f "$cert_pem_file" \
   -p "$cert_password" \
   --proxy "$proxy" \
   --uri "${base}ns#SportsCenter" \
   --label "Sports center" \
-  --slug sports-center \
   --sub-class-of "https://schema.org/CivicStructure" \
   "${base}admin/ontologies/namespace/"
 
-./add-class.sh \
+add-class.sh \
   -b "${base}admin/" \
   -f "$cert_pem_file" \
   -p "$cert_password" \
   --proxy "$proxy" \
   --uri "${base}ns#PublicToilet" \
   --label "Toilet" \
-  --slug public-toilet \
   --sub-class-of "http://schema.org/PublicToilet" \
   "${base}admin/ontologies/namespace/"
-
-popd
--- demo/city-graph/create-charts.sh
+++ demo/city-graph/create-charts.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-[ -z "$SCRIPT_ROOT" ] && echo "Need to set SCRIPT_ROOT" && exit 1;
-
 if [ "$#" -ne 3 ] && [ "$#" -ne 4 ]; then
   echo "Usage:   $0" '$base $cert_pem_file $cert_password [$proxy]' >&2
   echo "Example: $0" 'https://localhost:4443/ ../../../ssl/owner/cert.pem Password [https://localhost:5443/]' >&2
@@ -21,63 +19,37 @@ fi
 
 pwd=$(realpath "$PWD")
 
-pushd . && cd "$SCRIPT_ROOT"
-
-chart_container=$(./create-container.sh \
-  -b "$base" \
-  -f "$cert_pem_file" \
-  -p "$cert_password" \
-  --proxy "$proxy" \
-  --title "Charts" \
-  --slug "charts" \
-  --parent "$parent" \
-  --mode "https://w3id.org/atomgraph/client#MapMode"
-)
-
-parking_facilities_doc=$(./create-item.sh \
-  -b "${base}admin/" \
-  -f "$cert_pem_file" \
-  -p "$cert_password" \
-  --proxy "$proxy" \
-  --title "Parking facilities" \
-  --slug "parking-facilities" \
-  --container "$chart_container"
-)
-
-./add-select.sh  \
+chart_doc=$(create-item.sh \
   -b "$base" \
   -f "$cert_pem_file" \
   -p "$cert_password" \
   --proxy "$proxy" \
   --title "Largest parking facilities" \
-  --fragment "parking-facilities-by-spaces" \
-  --query-file "${pwd}/queries/charts/parking-facilities-by-spaces.rq"
-  "$parking_facilities_doc"
+  --slug "largest-parkings" \
+  --container "${base}charts/"
+)
 
-pushd . > /dev/null && cd "$SCRIPT_ROOT"
+query_id="select-largest-parkings"
 
-query_ntriples=$(./get.sh \
-  -f "$cert_pem_file" \
-  -p "$cert_password" \
-  --proxy "$proxy" \
-  --accept 'application/n-triples' \
-  "$query_doc")
-
-popd > /dev/null
-
-query=$(echo "$query_ntriples" | sed -rn "s/<${query_doc//\//\\/}> <http:\/\/xmlns.com\/foaf\/0.1\/primaryTopic> <(.*)> \./\1/p" | head -1)
-
-./create-result-set-chart.sh \
+add-select.sh  \
   -b "$base" \
   -f "$cert_pem_file" \
   -p "$cert_password" \
   --proxy "$proxy" \
   --title "Largest parking facilities" \
-  --slug largest-parkings \
-  --query "$query" \
+  --fragment "$query_id" \
+  --query-file "${pwd}/queries/charts/parking-facilities-by-spaces.rq" \
+  "$chart_doc"
+
+add-result-set-chart.sh \
+  -b "$base" \
+  -f "$cert_pem_file" \
+  -p "$cert_password" \
+  --proxy "$proxy" \
+  --title "Largest parking facilities" \
+  --fragment this \
+  --query "${chart_doc}#${query_id}" \
   --chart-type "https://w3id.org/atomgraph/client#BarChart" \
   --category-var-name "name" \
-  --series-var-name "spaces"
-#  --fragment this \
-
-popd
+  --series-var-name "spaces" \
+  "$chart_doc"
